043495037bf067484e3f66d1c827679b58763c0a
kholilboy <kholil.zboy@gmail.com>
2020-11-10 01:23:05 +0700

    Finally Done

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+<div id="app">
+</div>
+</body>
+<script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+</html>
